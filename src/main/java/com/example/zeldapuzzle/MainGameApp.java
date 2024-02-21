ceec18a3f6129e0b08718403fc83c08a9fbfc015
BouMas2231020 <mbournane7@gmail.com>
2024-02-21 18:34:55 -0500

    Masten le big daron

--- src/main/java/com/example/zeldapuzzle/MainGameApp.java
+++ src/main/java/com/example/zeldapuzzle/MainGameApp.java
@@ -2,14 +2,23 @@ package com.example.zeldapuzzle;
 
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.dsl.components.OffscreenCleanComponent;
 import com.almasb.fxgl.dsl.components.OffscreenInvisibleComponent;
 import com.almasb.fxgl.dsl.components.OffscreenPauseComponent;
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.input.Input;
 import com.almasb.fxgl.input.UserAction;
+import com.almasb.fxgl.texture.Texture;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 import static com.almasb.fxgl.dsl.FXGL.*;
 
@@ -67,12 +76,22 @@ public class MainGameApp extends GameApplication {
 
     @Override
     protected void initGame(){
-
-    player = entityBuilder()
+        Image image = new Image("https://w7.pngwing.com/pngs/810/275/png-transparent-the-legend-of-zelda-a-link-to-the-past-princess-zelda-zelda-ii-the-adventure-of-link-pixel-toys-rectangle-symmetry-video-game.png");
+        Image image3 = null;
+        try {
+            image3 = new Image(new FileInputStream("src/main/resources/assets/textures/zelda1.png"));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        Image image2 = new Image("https://i.seadn.io/gae/kpoyimC6bMVwC27nbg13G7vHxIj4adgVK9gCSnZQOCIgeuOsuxSXboKOWpqnI4KDFOIiYNx6Woci6Oh9VK2YzdIRdi347nc5KI9vbg?auto=format&dpr=1&w=1000");
+        ImageView imageView = new ImageView(image3);
+    player = FXGL.entityBuilder()
             .at(100,100)
-            .viewWithBBox("com/example/zeldapuzzle/zelda.png")
+            .view(imageView)
+            .scale(0.2,0.2)
             .with(new Player())
             .buildAndAttach();
+
     }
 
     @Override
--- src/main/java/com/example/zeldapuzzle/Player.java
+++ src/main/java/com/example/zeldapuzzle/Player.java
@@ -1,9 +1,24 @@
 package com.example.zeldapuzzle;
 
+import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.component.Component;
 
 public class Player extends Component {
+
+
     private double speed = 2;
+
+
+    @Override
+    public void onAdded() {
+
+
+    }
+
+    @Override
+    public void onUpdate(double tpf) {
+        super.onUpdate(tpf);
+    }
 
     public void moveRight(){
         entity.translateX(speed);
